17dbc09ce85877d77200afc15c7f27bbb0667687
unow1992@gmail.com <unow1992@gmail.com>
2020-10-27 20:09:40 +0900

    Webpage modification

---
+++ scroll.js
@@ -0,0 +1,28 @@
+function goToWorks() {
+    var elmnt = document.getElementById("works");
+    elmnt.scrollIntoView({
+        behavior:'smooth', 
+        block: 'center',
+    });
+}
+function goToSkills() {
+    var elmnt = document.getElementById("skills");
+    elmnt.scrollIntoView({
+        behavior:'smooth',
+        block: 'center'
+    });
+}
+function goToLanguage() {
+    var elmnt = document.getElementById("language");
+    elmnt.scrollIntoView({
+        behavior:'smooth',
+        block: 'center'
+    });
+}
+function goToContact() {
+    var elmnt = document.getElementById("contact");
+    elmnt.scrollIntoView({
+        behavior:'smooth',
+        block:'center'
+    });
+}
