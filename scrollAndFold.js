91f8b8711dc50cd2a0b9405bb278c59d2595520d
unow1992@gmail.com <unow1992@gmail.com>
2020-11-13 18:14:03 +0900

    add responsive  function

--- scrollAndFold.js
+++ scrollAndFold.js
@@ -1,28 +1,38 @@
+const toggleButton = document.querySelector('.navToggleButton');
+const image = document.querySelector('.navImage');
+const menu = document.querySelector('.navMenu');
+
+toggleButton.addEventListener('click',()=>{
+    menu.classList.toggle('active');
+    image.classList.toggle('active');
+    toggleButton.classList.toggle('unfolded')
+});
+
 function goToWorks() {
     var elmnt = document.getElementById("works");
     elmnt.scrollIntoView({
         behavior:'smooth', 
-        block: 'center',
+        block: 'start'
     });
 }
 function goToSkills() {
     var elmnt = document.getElementById("skills");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block: 'center'
+        block: 'start'
     });
 }
 function goToLanguage() {
     var elmnt = document.getElementById("language");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block: 'center'
+        block: 'start'
     });
 }
 function goToContact() {
     var elmnt = document.getElementById("contact");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block:'center'
+        block:'start'
     });
 }
