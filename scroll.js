86fa387392d6ad07fe62cafe194e45f92c30dc93
unow0517 <unow1992@gmail.com>
2020-11-17 21:14:47 +0100

    Add Nweeter

--- scroll.js
+++ scroll.js
@@ -2,27 +2,27 @@ function goToWorks() {
     var elmnt = document.getElementById("works");
     elmnt.scrollIntoView({
         behavior:'smooth', 
-        block: 'end',
+        block: 'start',
     });
 }
 function goToSkills() {
     var elmnt = document.getElementById("skills");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block: 'center'
+        block: 'start'
     });
 }
 function goToLanguage() {
     var elmnt = document.getElementById("language");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block: 'center'
+        block: 'start'
     });
 }
 function goToContact() {
     var elmnt = document.getElementById("contact");
     elmnt.scrollIntoView({
         behavior:'smooth',
-        block:'center'
+        block:'start'
     });
 }
