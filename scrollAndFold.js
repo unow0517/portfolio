3d3e43d53c55200ddc6e63db6448aa2a0061eb58
unow0517 <unow1992@gmail.com>
2022-05-14 16:55:07 +0200

    body size, font changed with bootstrap

--- scrollAndFold.js
+++ scrollAndFold.js
@@ -1,46 +1,46 @@
-const toggleButton = document.querySelector('.navToggleButton');
+// const toggleButton = document.querySelector('.navToggleButton');
 const image = document.querySelector('.navImage');
 const menu = document.querySelector('.navMenu');
 
-toggleButton.addEventListener('click',()=>{
-    menu.classList.toggle('active');
-    image.classList.toggle('active');
-    toggleButton.classList.toggle('unfolded')
-});
+// toggleButton.addEventListener('click',()=>{
+//     menu.classList.toggle('active');
+//     image.classList.toggle('active');
+//     toggleButton.classList.toggle('unfolded')
+// });
 
 function goToMainTitle() {
-    var elmnt = document.getElementById("maintitle");
-    elmnt.scrollIntoView({
-        behavior:'smooth', 
-        block: 'center'
-    });
+  var elmnt = document.getElementById("maintitle");
+  elmnt.scrollIntoView({
+    behavior: 'smooth',
+    block: 'center'
+  });
 }
 
 function goToWorks() {
-    var elmnt = document.getElementById("works");
-    elmnt.scrollIntoView({
-        behavior:'smooth', 
-        block: 'start'
-    });
+  var elmnt = document.getElementById("works");
+  elmnt.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
 }
 function goToSkills() {
-    var elmnt = document.getElementById("skills");
-    elmnt.scrollIntoView({
-        behavior:'smooth',
-        block: 'start'
-    });
+  var elmnt = document.getElementById("skills");
+  elmnt.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
 }
 function goToLanguage() {
-    var elmnt = document.getElementById("language");
-    elmnt.scrollIntoView({
-        behavior:'smooth',
-        block: 'start'
-    });
+  var elmnt = document.getElementById("language");
+  elmnt.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
 }
 function goToContact() {
-    var elmnt = document.getElementById("contact");
-    elmnt.scrollIntoView({
-        behavior:'smooth',
-        block:'start'
-    });
+  var elmnt = document.getElementById("contact");
+  elmnt.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
 }
